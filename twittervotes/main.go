d3c58dbd37a7c9cce7a49ded5b351a0cc9b6c9d0
Ubuntu <manjeets@gomicro.iria1um3n51ehbukxaulvw0nte.dx.internal.cloudapp.net>
2019-06-16 02:27:15 +0000

    initial social poll

---
+++ twittervotes/main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+    "log"
+    "sync"
+    "syscall"
+    "os/signal"
+    "time"
+    "github.com/nsqio/go-nsq"
+    "os"
+    "gopkg.in/mgo.v2"
+)
+
+var db *mgo.Session
+
+func dialdb() error {
+	var err error
+	log.Println("dialing mongodb: localhost")
+	db, err = mgo.Dial("localhost")
+	return err
+}
+func closedb() {
+	db.Close()
+	log.Println("closed database connection")
+}
+
+type poll struct {
+	Options []string
+}
+
+func loadOptions() ([]string, error) {
+	var options []string
+	iter := db.DB("ballots").C("polls").Find(nil).Iter()
+	var p poll
+	for iter.Next(&p) {
+		options = append(options, p.Options...)
+	}
+	iter.Close()
+	return options, iter.Err()
+}
+func publishVotes(votes <-chan string) <-chan struct{} {
+	stopchan := make(chan struct{}, 1)
+	pub, _ := nsq.NewProducer("localhost:4150",
+		nsq.NewConfig())
+	go func() {
+		for vote := range votes {
+			pub.Publish("votes", []byte(vote)) // publish vote
+		}
+		log.Println("Publisher: Stopping")
+		pub.Stop()
+		log.Println("Publisher: Stopped")
+		stopchan <- struct{}{}
+	}()
+	return stopchan
+}
+
+func main() {
+	var stoplock sync.Mutex // protects stop
+	stop := false
+	stopChan := make(chan struct{}, 1)
+	signalChan := make(chan os.Signal, 1)
+	go func() {
+		<-signalChan
+		stoplock.Lock()
+		stop = true
+		stoplock.Unlock()
+		log.Println("Stopping...")
+		stopChan <- struct{}{}
+		closeConn()
+	}()
+	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
+	if err := dialdb(); err != nil {
+		log.Fatalln("failed to dial MongoDB:", err)
+	}
+	defer closedb()
+	votes := make(chan string) // chan for votes
+	publisherStoppedChan := publishVotes(votes)
+	twitterStoppedChan := startTwitterStream(stopChan, votes)
+	go func() {
+		for {
+			time.Sleep(1 * time.Minute)
+			closeConn()
+			stoplock.Lock()
+			if stop {
+				stoplock.Unlock()
+				return
+			}
+			stoplock.Unlock()
+		}
+	}()
+	<-twitterStoppedChan
+	close(votes)
+	<-publisherStoppedChan
+}
